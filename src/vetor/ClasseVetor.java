0232967ff057c8599eed77d276fedce483a23a73
Rafael Laba <Rafael Laba@192.168.3.103>
2018-03-28 20:05:16 -0300

    Criação da Classe Vetor

--- src/vetor/ClasseVetor.java
+++ src/vetor/ClasseVetor.java
@@ -10,5 +10,58 @@ package vetor;
  * @author Rafael Laba
  */
 public class ClasseVetor {
+       int v[];
     
+    
+    /* Construtor */
+    public ClasseVetor(int size){
+       v = new int[size];
+    }
+
+    /* Preenche o vetor com valores aleatórios*/
+    public void randomFill(){
+        
+    }
+
+    /* Implementa o método de ordenação bubbleSort*/
+    public void bubbleSort(){
+    
+    }
+
+    /* Converte o vetor para String*/
+    public String toString(){
+            return null;
+    
+    }
+
+    /* Compara se dois vetores são iguais */
+    public boolean equals(int[] v){
+            return false;
+    
+    }
+
+    /* Atribui um valor na posição pos do vetor */
+    public void setAt(int pos, int value){
+    
+    }
+
+    /* Retorna o valor na posição pos do vetor. */
+    public int valueAt(int pos){
+            return pos;
+    
+    }
+
+    /* Retorna o tamanho do vetor */
+    public Vetor length(){
+            return v;
+    
+    }
+
+    /* Concatena dois vetores */
+    public Vetor Concat(Vetor v){
+        this.setAt(0, 0);
+            return null;
+    }
+
+
 }
--- src/vetor/Vetor.java
+++ src/vetor/Vetor.java
@@ -7,7 +7,15 @@ package vetor;
 public class Vetor {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        Vetor v = new Vetor(10);
+        Vetor v2 = new Vetor(5);
+    
+        
+    v.randomFill();
+    v2.randomFill();
+    v2 = v2.Concat(v);
+    v2.bubbleSort();
+    System.out.println(v2.toString());
     }
     
 }
